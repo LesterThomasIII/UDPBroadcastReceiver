747df1ea07fd5f82351ef7d072e0fd621a3f1d0b
Lester Thomas <edward.thomas@salientcrgt.com>
2021-10-02 13:25:28 -0500

    Updated placement of Socket broadcastReceiver in the code, needed to bind before other call in the program are used

--- Program.cs
+++ Program.cs
@@ -22,54 +22,56 @@ namespace UDPBroadcastReceiver
             //IPEndpoints need 2 parameters
             //IPAddress designed to receive specific or any IP addresses
             //23000 is the port that the receiver is listening on the receiver must be on the same port received
-            IPEndPoint ipEndPointLocal = new IPEndPoint(IPAddress.Any, 23000);
-           //this Identifies the Ip Sender to validate they are acceptable
-           IPEndPoint ipEpSender = new IPEndPoint(IPAddress.Any, 0);
-           //this establishes who the Sender is and applies it to a string variable for reference
-           EndPoint eSender = (EndPoint)ipEpSender;
+            IPEndPoint ipEpLocal = new IPEndPoint(IPAddress.Any, 23000);
             //byte arrays are used to receive data 
             byte[] receiveBuffer = new byte[512];
             //generate the string for inputting the data
-            string dataReceived = String.Empty;
-            
-            //we must bind the socket on the machine
-            //try is used to ID if there is a problem with this
-            
+            string dataRcvd= String.Empty;
+            //this Identifies the Ip Sender to validate they are acceptable
+            IPEndPoint ipEpSender = new IPEndPoint(IPAddress.Any, 0);
+           //this establishes who the Sender is and applies it to a string variable for reference
+           EndPoint eSender = (EndPoint)ipEpSender;
+           //values for standard uses
+          
+
+          
+            //try is used to ID if there is a problem with the connection!
             try
             {
-                //values for standard uses
+                //we must bind the socket on the machine
                 //used to bind the socket to the local IP Endpoint
-                sockBroadCastReceiver.Bind(ipEndPointLocal);
-               //place this into an infinite loop to constantly receive info
-               while (true)
+                sockBroadCastReceiver.Bind(ipEpLocal);
+               
+                //place this into an infinite loop to constantly receive info
+                while (true)
                {
-                   int nCountReceived = 0;
-                   int ipSender = 0;
-                   //receive into the buffer the amount of data and returns an integer
-                    nCountReceived = sockBroadCastReceiver.Receive(receiveBuffer);
-                   //this shows the received from info
-                     ipSender = sockBroadCastReceiver.ReceiveFrom(receiveBuffer, ref eSender);
+                   //this shows the received from info and takes 2 parameters
+                   //the byte array buffer 
+                   //and the EndPointSender
+                   int ipSender = sockBroadCastReceiver.ReceiveFrom(receiveBuffer, ref eSender);
+
+                    //receive into the buffer the amount of data and returns an integer
+                    int nCRcv = sockBroadCastReceiver.Receive(receiveBuffer);
                    //this takes in the data and converts it to readable info in ACSII format
                    //this takes 3 parameters
                    //bytes is # of Bytes from the buffer array //!receiveBuffer!//
                    //Index is the index starting point for the conversion (i.e.0) //!0!//
                    //count is the count of bytes in the broadcast the number of bytes to convert in the broadcast //!nCountReceived!//
-                   dataReceived = Encoding.ASCII.GetString(receiveBuffer, 0, nCountReceived);
+                   dataRcvd = Encoding.ASCII.GetString(receiveBuffer, 0, nCRcv);
                    //ouput and validate if the data has been received
-                   Console.WriteLine(
-                       "# Bytes Received: " + nCountReceived + " Information Transferred: " + dataReceived);
-                   //TODO This one was acting Weird, will work on this some more
-                   //print out who sent it
-                  // Console.WriteLine("Received from: "+ ipSender.ToString());
-                  
+                   Console.WriteLine("# Bytes Rcv: " + nCRcv + Environment.NewLine
+                                     +"Info: " + dataRcvd+ Environment.NewLine+"from: " 
+                                     + eSender.ToString());
+
                    //clears the array receiveBuffer once data has been received
                    Array.Clear(receiveBuffer, 0, receiveBuffer.Length);
 
-                   //echo function to send message along another path
-                   if (!dataReceived.Equals("<ECHO>")) continue;
+                 /*  // TODO echo function to send message along another path
+                   if (!dataRcvd.Equals("<ECHO>")) continue;
                    //this rebroadcasts the info back to the sender
-                   sockBroadCastReceiver.SendTo(receiveBuffer, 0, nCountReceived, SocketFlags.None, eSender);
+                   sockBroadCastReceiver.SendTo(receiveBuffer, 0, nCRcv, SocketFlags.None, eSender);
                    Console.WriteLine(" Echoed Back ");
+                 */
 
                }
             }
